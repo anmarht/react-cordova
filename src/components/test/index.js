b15a06ec32bd1d43ac0039a2dd90a91c9e6dcd24
Unknown <anmarhasan@gmail.com>
2020-11-22 19:01:45 -0800

    React On Cordova

---
+++ src/components/test/index.js
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button';
+
+
+const Test = () => {
+
+    return (
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+    )
+};
+
+export default Test;
